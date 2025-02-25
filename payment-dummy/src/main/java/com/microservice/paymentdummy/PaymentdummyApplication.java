30beb5769b740f0729430392172206fbfcb9e7e7
ridwan zan haqqi <ridwan.haqi@gmail.com>
2025-02-26 00:50:28 +0700

    Perubahan

--- payment-dummy/src/main/java/com/microservice/paymentdummy/PaymentdummyApplication.java
+++ payment-dummy/src/main/java/com/microservice/paymentdummy/PaymentdummyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentDummyApplication {
+public class PaymentdummyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentDummyApplication.class, args);
+		SpringApplication.run(PaymentdummyApplication.class, args);
 	}
 
 }
--- payment-dummy/src/test/java/com/microservice/paymentdummy/PaymentdummyApplicationTests.java
+++ payment-dummy/src/test/java/com/microservice/paymentdummy/PaymentdummyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaymentDummyApplicationTests {
+class PaymentdummyApplicationTests {
 
 	@Test
 	void contextLoads() {
